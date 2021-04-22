6b4148511a3fcf84a9bcd6fe27dfaafdaefc45f0
sucharita05 <68145021+sucharita05@users.noreply.github.com>
2021-04-22 23:39:21 +0530

    changed setup.sh

--- back_end/setup.sh
+++ back_end/setup.sh
@@ -2,4 +2,4 @@ export AUTH0_DOMAIN='starinmaking.us.auth0.com'
 export ALGORITHMS=['RS256']
 export API_AUDIENCE='http://localhost:5000'
 export SECRET='jC905OeKFFgTOCas_nxrjMH5rkGcFo9P1IDBlrnKAsDsdQH2PiJE6fuYSMkXJ2Or'
-export DATABASE_URL='postgres://vemzpndanyxaft:6bca144dad71cdbfa0c888ece7d91b5272f40493efc7277d4aee339782844f0a@ec2-54-163-254-204.compute-1.amazonaws.com:5432/d20ksd0amd8ivm'
+export DATABASE_URL='postgres://jdbfzzszxeykob:4713bbf946a0b2da2841d6ca3ae7caad64a376e81ecb7b2bc8753dc2370de938@ec2-54-90-211-192.compute-1.amazonaws.com:5432/d91pgsbckonthj'
